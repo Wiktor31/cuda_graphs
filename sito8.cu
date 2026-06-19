9cbb31844ee593e0b675622b880fb318653b532b
Wiktor Mazanka <wiktor.mazanka1234@interia.pl>
2026-06-19 11:01:14 +0200

    2

--- sito8.cu
+++ sito8.cu
@@ -156,7 +156,6 @@ __global__ void test(char * BUFFOR1,int len,int print_if,int limit) {
   //for (i=0;i<=n;i++) printf("%f ",x[i]);
  
   //printf("GPU:[%s]\n",BUFFOR);
-  printf("%s\n",BUFFOR);
   if (print_if>0)
   printf("%s\n",BUFFOR);
 }
