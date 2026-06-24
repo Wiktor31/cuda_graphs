422514e7957741d27f39f6f464b6eb67005ce403
Wiktor Mazanka <wiktor.mazanka1234@interia.pl>
2026-06-24 14:57:25 +0200

    2

--- sito8.cu
+++ sito8.cu
@@ -288,9 +288,9 @@ int main(int argc, char *argv[])
   char * cuda_bufor;
   int print_if = 1,omp_use=1; 
   int show_1 = 1,blokow=1,watkow=1;
-  if (argc>1) {print_if=strtol(argv[1],NULL,10);}  
-  if (argc>2) {blokow=strtol(argv[2],NULL,10);}  
-  if (argc>3) {watkow=strtol(argv[3],NULL,10);}  
+  if (argc>1) {blokow=strtol(argv[1],NULL,10);}  
+  if (argc>2) {watkow=strtol(argv[2],NULL,10);}  
+  if (argc>3) {print_if=strtol(argv[3],NULL,10);}  
   if (argc>4) {show_1=strtol(argv[4],NULL,10);}  
  
   int i = 0,j=0,len;
