49c1d0482f7e5d040f7256c65beb7d3c35f58915
Wiktor Mazanka <wiktor.mazanka1234@interia.pl>
2026-06-24 14:56:21 +0200

    2

--- sito8.cu
+++ sito8.cu
@@ -291,7 +291,7 @@ int main(int argc, char *argv[])
   if (argc>1) {print_if=strtol(argv[1],NULL,10);}  
   if (argc>2) {blokow=strtol(argv[2],NULL,10);}  
   if (argc>3) {watkow=strtol(argv[3],NULL,10);}  
-  if (argc>4) {show_1=strtol(argv[3],NULL,10);}  
+  if (argc>4) {show_1=strtol(argv[4],NULL,10);}  
  
   int i = 0,j=0,len;
   cudaMalloc((void**)&cuda_bufor,BUFSIZE2);
